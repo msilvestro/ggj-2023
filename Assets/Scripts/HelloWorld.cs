cd2f6214528653cd2424d303c5ce768dc5b101b9
Matteo Silvestro <matteosilvestro@live.it>
2023-02-03 18:39:58 +0100

    Create basic project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+}
