f50bc0237c9ed86a6897fccef4af94e8a7749b13
Matteo Silvestro <matteosilvestro@live.it>
2023-02-03 21:05:33 +0100

    Add simple player movement

---
+++ Assets/Scripts/Root.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace GGJ23
+{
+    public class Root : MonoBehaviour
+    {
+        [SerializeField]
+        private float moveSpeed = 1f;
+
+        private Rigidbody rb;
+
+        private Vector3 movementInput;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            float horizontalDelta = Input.GetAxis("Horizontal");
+            float verticalDelta = Input.GetAxis("Vertical");
+            movementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 moveVector = transform.TransformDirection(movementInput) * moveSpeed;
+            rb.velocity = new Vector3(moveVector.x, rb.velocity.y, moveVector.z);
+        }
+    }
+}
