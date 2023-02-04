ecefe2c837394f77494397dc995c5ea450696976
Matteo Silvestro <matteosilvestro@live.it>
2023-02-04 22:15:49 +0100

    Keep track of years per feature creep type

--- Assets/Scripts/DelayScore.cs
+++ Assets/Scripts/DelayScore.cs
@@ -1,19 +1,32 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace GGJ23
 {
     public class DelayScore : MonoBehaviour
     {
-        [SerializeField]
         private int yearsOfDelay = 0;
 
         [SerializeField]
         private FeatureCreepCollection featureCreepCollection;
 
+        [SerializeField]
+        private int yearsToAdd;
+
         public event Action<int> OnYearsOfDelayChange;
         public event Action<string, string> OnDelay;
+
+        [SerializeField]
+        private Dictionary<string, int> featureCreepToYears = new();
+
+        private void Awake()
+        {
+            foreach (FeatureCreepData featureCreepData in featureCreepCollection.featureCreepData)
+            {
+                featureCreepToYears.Add(featureCreepData.title, 0);
+            }
+        }
 
         private void Start()
         {
@@ -28,9 +41,12 @@ namespace GGJ23
 
         public void Delay()
         {
-            AddYearsOfDelay(100);
+            AddYearsOfDelay(yearsToAdd);
             FeatureCreepData featureCreepData = featureCreepCollection.GetRandomFeatureCreepData();
-            OnDelay.Invoke(featureCreepData.title, featureCreepData.GetRandomDescription());
+            string title = featureCreepData.title;
+            string description = featureCreepData.GetRandomDescription();
+            OnDelay.Invoke(title, description);
+            featureCreepToYears[title] += yearsToAdd;
         }
     }
 }
