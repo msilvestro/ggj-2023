1766c7b1d5c7ceac683b7a21b536d2cb47d48aaf
Matteo Silvestro <matteosilvestro@live.it>
2023-02-04 14:41:17 +0100

    Add impulse to developers

--- Assets/Scripts/Hittable.cs
+++ Assets/Scripts/Hittable.cs
@@ -7,17 +7,35 @@ namespace GGJ23
         [SerializeField]
         private int segmentsToAddOnHit = 5;
 
+        [SerializeField]
+        private float hitForceMagnitude = 10f;
+
         private int timesHasBeenHit = 0;
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
 
         public void Hit(out bool isFirstTime)
         {
             timesHasBeenHit += 1;
             isFirstTime = timesHasBeenHit == 1;
+            ThrowInTheAir();
         }
 
         public int GetSegmentsToAddOnHit()
         {
             return segmentsToAddOnHit;
+        }
+
+        private void ThrowInTheAir()
+        {
+            rb.AddForce(
+                new Vector3(Random.Range(-1f, 1f), 1, Random.Range(-1f, 1f)) * hitForceMagnitude,
+                ForceMode.Impulse
+            );
         }
     }
 }
